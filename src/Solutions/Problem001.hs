aaf656de32ac99ac137936f6e232a60be6c36892
Johan Ask <jhnesk@gmail.com>
2015-09-20 17:58:43 +0200

    Formatting comments for shorter lines

--- src/Solutions/Problem001.hs
+++ src/Solutions/Problem001.hs
@@ -3,12 +3,15 @@ Module      : $Header$
 Description : Solution to problem 1
 License     : PublicDomain
 
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
 Find the sum of all the multiples of 3 or 5 below 1000.
 -}
 module Solutions.Problem001 where
 
--- | The solution sums all numbers in the list of natural numbers below n filtered by divisibility for 3 and 5.
+-- | The solution sums all numbers in the list of natural numbers below n
+--   filtered by divisibility for 3 and 5.
 solution :: Int -> Int
 solution n = sum (filter condition [0..n-1])
     where
