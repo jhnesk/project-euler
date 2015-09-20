7a7aa41119c019fa000042b46e7b00f9565b4049
Johan Ask <jhnesk@gmail.com>
2015-09-20 23:15:39 +0200

    Solution to problem 4

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 import Solutions.Problem001 as P1
 import Solutions.Problem002 as P2
+import Solutions.Problem004 as P4
 
 main = do
     print (P1.solution 1000)
     print (P2.solution 4000000)
+    print (P4.solution)
 
