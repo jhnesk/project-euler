5381ecc2be785c84a50bc70b6568dc4c4cd0c6d7
Johan Ask <jhnesk@gmail.com>
2015-09-21 19:18:30 +0200

    Naive solution to problem 5

---
+++ src/Solutions/Problem005.hs
@@ -0,0 +1,19 @@
+{- |
+Module      : $Header$
+Description : Solution to problem 5
+License     : PublicDomain
+
+2520 is the smallest number that can be divided by each of the numbers
+from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of
+the numbers from 1 to 20?
+-}
+module Solutions.Problem005 where
+import Data.List(find)
+
+-- |
+solution n = find (divisibleByAll [1..n]) [n..]
+    where
+        divisibleByAll [] n = True
+        divisibleByAll (x:xs) n = n `mod` x == 0 && divisibleByAll xs n
