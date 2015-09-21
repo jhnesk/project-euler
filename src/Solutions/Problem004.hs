8bf589c228f325889e8f5adccaf63b393767f2a5
Johan Ask <jhnesk@gmail.com>
2015-09-21 19:19:27 +0200

    Clean up problem 4

--- src/Solutions/Problem004.hs
+++ src/Solutions/Problem004.hs
@@ -9,12 +9,11 @@ made from the product of two 2-digit numbers is 9009 = 91 × 99.
 Find the largest palindrome made from the product of two 3-digit numbers.
 -}
 module Solutions.Problem004 where
-import Data.List(find)
 
 -- | Take two lists of all 3-digit numbers and multiply the elements.
 --   Filter for numbers that is the same string backwards and return
 --   the maximum value.
-solution = maximum (filter palindrome (product [999,998..100] [999,998..100]))
+solution = maximum (filter palindrome (product [100..999] [100..999]))
    where
         product xs ys = [x*y | x <- xs, y <-ys]
         palindrome x = (show x) == reverse (show x)
